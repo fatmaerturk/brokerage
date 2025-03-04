35fed0ff9bfe81f4f06826f625f2dda3ca93f4bc
Fatma <fatmaerturkuks@gmail.com>
2025-03-05 02:13:16 +0300

    Asset controller updated

--- src/main/java/com/ing/brokerage/controller/AssetController.java
+++ src/main/java/com/ing/brokerage/controller/AssetController.java
@@ -4,32 +4,19 @@ import com.ing.brokerage.entity.Asset;
 import com.ing.brokerage.service.AssetService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-/**
- * Controller for asset-related endpoints.
- */
 @RestController
 @RequestMapping("/assets")
 public class AssetController {
-
     private final AssetService assetService;
-
     public AssetController(AssetService assetService) {
         this.assetService = assetService;
     }
-
-    /**
-     * Endpoint to list all assets for the authenticated customer.
-     */
     @GetMapping("/list")
     public ResponseEntity<List<Asset>> listAssets(@RequestParam String customerId) {
         List<Asset> assets = assetService.listAssets(customerId);
         return new ResponseEntity<>(assets, HttpStatus.OK);
     }
-
-
 }
--- src/main/java/com/ing/brokerage/entity/Asset.java
+++ src/main/java/com/ing/brokerage/entity/Asset.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,6 +17,7 @@ import lombok.Setter;
 @Table(name = "assets")
 @Getter
 @Setter
+@AllArgsConstructor
 @JsonIgnoreProperties("customer")
 public class Asset {
 
